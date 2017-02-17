00509008b3fcdfaa73dd5841b7ae0378e62fe5b5
ChenZhen <aishenhaowe@sina.com>
2017-02-17 22:06:14 +0800

    SV-27:ZZ

---
+++ Main/src/io-server/inc/Server.h
@@ -0,0 +1,77 @@
+/******************************************************************************
+
+                  版权所有 (C), 2001-2011, IOT CopyRight
+
+ ******************************************************************************
+  文 件 名   : Server.h
+  版 本 号   : 初稿
+  作    者   : ChenZhen
+  生成日期   : 2017年2月17日
+  最近修改   :
+  功能描述   : Server.cc 的头文件
+  函数列表   :
+  修改历史   :
+  1.日    期   : 2017年2月17日
+    作    者   : ChenZhen
+    修改内容   : 创建文件
+
+******************************************************************************/
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+
+/*----------------------------------------------*
+ * 包含头文件                                   *
+ *----------------------------------------------*/
+#include "InnerHandleThread.h"
+#include "OuterHandleThread.h"
+#include "OuterTimerThread.h"
+
+using namespace std;
+
+
+/*----------------------------------------------*
+ * 类声明                                       *
+ *----------------------------------------------*/
+class Server
+{
+/* Public Methods */
+public:
+    /* 构造函数 */
+    Server();
+    
+    /* 析构函数 */
+    virtual ~Server();
+
+    /* 获取Tag */
+    int GetTag();
+
+    /* 初始化 */
+    int Init(string configPath);
+
+    /* 启动服务器 */
+    int Run();
+
+/* Public Members */
+public:
+    InnerHandleThread       *m_InnerHandleThread;
+    OuterHandleThread       *m_OuterHandleThread;
+    OuterTimerThread        *m_OuterTimerThread;
+
+/* Protected Methods */
+protected:
+
+/* Protected Members */
+protected:
+
+/* Private Methods */
+private:
+
+/* Private Members */
+private:
+    int                     m_tag;
+    string                  m_configPath;
+};
+
+
+#endif /* __SERVER_H__ */
--- Main/src/io-server/main.cc
+++ Main/src/io-server/main.cc
@@ -1,10 +1,17 @@
 #include <iostream>
 
+#include "Server.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
 	cout << "Io-Server start" << endl;
+
+    Server server = Server();
+
+    server.Init("qqq");
+    server.Run();
 
 	return 0;
 }
